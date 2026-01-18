246819da3f2b805367f3e885ccfd1c227636b482
bruce-mig <brucetmig@icloud.com>
2026-01-18 22:37:44 +0200

    added EnableResilientMethods annotation

--- src/main/java/com/github/bruce_mig/quick_bites/Application.java
+++ src/main/java/com/github/bruce_mig/quick_bites/Application.java
@@ -2,8 +2,10 @@ package com.github.bruce_mig.quick_bites;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.resilience.annotation.EnableResilientMethods;
 
 @SpringBootApplication
+@EnableResilientMethods
 public class Application {
 
 	public static void main(String[] args) {
